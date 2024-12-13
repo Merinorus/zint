993af53a7c81a5f509ff862ac3efa79f49239df4
Antoine Merino <antoine.merino@medadom.com>
2024-12-13 19:50:01 +0100

    DX Edge: add error codes

--- backend/dxfilmedge.c
+++ backend/dxfilmedge.c
@@ -64,8 +64,76 @@ void str_to_uppercase(char *str) {
     }
 }
 
+
+// /* Warning and error conditions (API return values) */
+// #define ZINT_WARN_HRT_TRUNCATED     1   /* Human Readable Text was truncated (max 199 bytes) */
+// #define ZINT_WARN_INVALID_OPTION    2   /* Invalid option given but overridden by Zint */
+// #define ZINT_WARN_USES_ECI          3   /* Automatic ECI inserted by Zint */
+// #define ZINT_WARN_NONCOMPLIANT      4   /* Symbol created not compliant with standards */
+// #define ZINT_ERROR                  5   /* Warn/error marker, not returned */
+
+// #define ZINT_ERROR_INVALID_CHECK    7   /* Input check digit incorrect */
+// #define ZINT_ERROR_INVALID_OPTION   8   /* Incorrect option given */
+// #define ZINT_ERROR_ENCODING_PROBLEM 9   /* Internal error (should not happen) */
+// #define ZINT_ERROR_FILE_ACCESS      10  /* Error opening output file */
+// #define ZINT_ERROR_MEMORY           11  /* Memory allocation (malloc) failure */
+// #define ZINT_ERROR_FILE_WRITE       12  /* Error writing to output file */
+// #define ZINT_ERROR_USES_ECI         13  /* Error counterpart of warning if WARN_FAIL_ALL set (see below) */
+// #define ZINT_ERROR_NONCOMPLIANT     14  /* Error counterpart of warning if WARN_FAIL_ALL set */
+// #define ZINT_ERROR_HRT_TRUNCATED    15  /* Error counterpart of warning if WARN_FAIL_ALL set */
+
+
+
+
+
+
+// #define ZINT_ERROR_TOO_LONG         5   /* Input data wrong length */
+// #define ZINT_ERROR_INVALID_DATA     6   /* Input data incorrect */
+
+
+
+// /* Set `symbol->errtxt` to "err_id: msg" with restricted subset of `printf()` formatting, returning `error_number`.
+//    If `err_id` is -1, the "err_id: " prefix is omitted. Only the following specifiers are supported: "c", "d", "f",
+//    "g" and "s", with no modifiers apart from "<n>$" numbering for l10n ("<n>" 1-9), in which case all specifiers must
+//    be numbered, "%s" with length precisions: "%.*s", "%<n+1>$.*<n>$s", "%.<p>s" and "%<n>$.<p>s", and "%d" with
+//    zero-padded minimum field lengths: "%0<m>d" or %<n>$0<m>d" ("<m>" 1-99) */
+// INTERNAL int errtxtf(const int error_number, struct zint_symbol *symbol, const int err_id, const char *fmt, ...) {
+//     const int max_len = (int) sizeof(symbol->errtxt) - 1;
+//     int p = errtxt_id_str(symbol->errtxt, err_id);
+//     const char *f;
+//     int i;
+//     int arg_cnt = 0;
+//     int have_num_arg = 0, have_unnum_arg = 0;
+//     va_list ap;
+//     int idxs[9] = {0}; /* Argument order */
+//     char specs[9] = {0}; /* Format specifiers */
+//     const char *ss[9] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL }; /* "%s" */
+//     int slens[9] = {0}; /* "%s" length precisions */
+//     int have_slens[9] = {0}; /* Bools for if "%s" has length precision */
+//     char dpads[9][3] = {{0}}; /* 2-digit minimum field length */
+//     char dfgs[9][100] = {{0}}; /* "%d", "%f" and "%g", allowing for padding up to 99 */
+//     int cs[9] = {0}; /* "%c" */
+
+
+    // if (length > 6) {
+    //     return errtxtf(ZINT_ERROR_TOO_LONG, symbol, 484, "Input length %d too long (maximum 6)", length);
+    // }
+    // if ((i = not_sane(NEON_F, source, length))) {
+    //     return errtxtf(ZINT_ERROR_INVALID_DATA, symbol, 485,
+    //                     "Invalid character at position %d in input (digits only)", i);
+    // }
+
+
+
+
+
+
+
+
+// Allowed formats: DX Codes parts 1 and 2 XXX-YY, 4-digit DX Extract or 6-digit full DX code.
+
 // /* Main parsing function */
-int parse_dx_code(const unsigned char *source, char *binary_output, int *output_length, bool *has_frame_info) {
+int parse_dx_code(struct zint_symbol *symbol, const unsigned char *source, char *binary_output, int *output_length, bool *has_frame_info) {
 
     int i;
     int parity_bit = 0;
@@ -78,23 +146,29 @@ int parse_dx_code(const unsigned char *source, char *binary_output, int *output_
     // TODO: check char array size
     unsigned char dx_info[MAX_DX_INFO_LENGTH] = "\0";
     unsigned char frame_info[MAX_FRAME_INFO_LENGTH] = "\0";
-
-    const unsigned char *frame_number_separator = strchr(source, '/');
-    if (frame_number_separator){
+    unsigned char *detected_char = strchr(source, ' ');
+    // Forbid spaces in the first part, because it could lead to mistakenly wrong barcodes
+    if (detected_char){
+        return errtxtf(ZINT_ERROR_INVALID_DATA, symbol, 1011, "Forbidden space at position %d (input: \"%s\").", detected_char - source + 1, source);
+    }
+    // Detect if there is the '/' separator, which indicates the frame number is present.
+    detected_char = strchr(source, '/');
+    if (detected_char){
         // Extract the part before "/" into dx_info    
-        size_t dx_length = frame_number_separator - source;
+        size_t dx_length = detected_char - source;
         printf("dx info stringlength: %d\n", dx_length);
         if (dx_length >= MAX_DX_INFO_LENGTH){
             fprintf(stderr, "Error: DX info too long.\n");
-            return -1;
+            // return errtxt(ZINT_ERROR_TOO_LONG, symbol, 357, "DX code is too long");
+            return errtxt(ZINT_ERROR_TOO_LONG, symbol, 1001, "DX part is too long.");
         }
         strncpy(dx_info, source, dx_length);
         dx_info[dx_length] = '\0';
         // Extract the part after "/" into frame_info
-        const unsigned char *frame_start = frame_number_separator + 1;
+        const unsigned char *frame_start = detected_char + 1;
         if (strlen(frame_start) >= MAX_FRAME_INFO_LENGTH) {
             fprintf(stderr, "Error: Frame info too long.\n");
-            return -1;
+            return errtxt(ZINT_ERROR_TOO_LONG, symbol, 1002, "Frame number part is too long.");
         }
         strcpy(frame_info, frame_start);
         *has_frame_info = true;
@@ -102,8 +176,7 @@ int parse_dx_code(const unsigned char *source, char *binary_output, int *output_
     else{
         // No "/" found, store the entire input in dx_info
         if (strlen(source) >= MAX_DX_INFO_LENGTH) {
-            fprintf(stderr, "Error: Input too long.\n");
-            return -1;
+            return errtxt(ZINT_ERROR_TOO_LONG, symbol, 1003, "DX part is too long.");
         }
         strcpy(dx_info, source);
     }
@@ -111,19 +184,27 @@ int parse_dx_code(const unsigned char *source, char *binary_output, int *output_
     if (has_frame_info){
         printf("Frame info part: %s\n", frame_info);
     }
-
     // Parse the dx_info
     if (strchr(dx_info, '-')){
-        printf("Found \"-\" separator, DX code 1 and 2 are separeted\n");
+        printf("Found \"-\" separator, DX code 1 and 2 are separated\n");
         // Format: DX part 1 and DX part 2, separated by a dash
         if (sscanf(dx_info, "%d-%d", &dx_code_1, &dx_code_2) < 2){
-            return -1; // invalid format
+            return errtxt(ZINT_ERROR_INVALID_DATA, symbol, 1004, "Wrong format for DX parts 1 and 2. Expected format: XXX-XX (digits).");
+        }
+        if (dx_code_1 <= 0 || dx_code_1 > 127){
+            return errtxt(ZINT_ERROR_INVALID_DATA, symbol, 1007, "DX part 1 must be between 1 and 127.");
+        }
+        if (dx_code_2 < 0 || dx_code_2 > 15){
+            return errtxt(ZINT_ERROR_INVALID_DATA, symbol, 1008, "DX part 2 must be between 0 and 15.");
         }
     }
     else{
         // Format: 4 digits (DX Extract) or 6 digits (DX Full)
-        if (strlen(dx_info) == 5 || strlen(dx_info) > 6){
-            return -1; // invalid format
+        if (strlen(dx_info) > 6){
+            return errtxt(ZINT_ERROR_TOO_LONG, symbol, 1005, "DX number is too long. Expected: 4 digits (DX extract) or 6 digits (DX full).");
+        }
+        else if (strlen(dx_info) == 5){
+            return errtxt(ZINT_ERROR_INVALID_DATA, symbol, 1006, "DX number is incorrect. Expected: 4 digits (DX extract) or 6 digits (DX full).");
         }
         if (strlen(dx_info) == 6){
             // DX Full. Remove first and last character
@@ -140,19 +221,17 @@ int parse_dx_code(const unsigned char *source, char *binary_output, int *output_
                 // if (sscanf(source, "%d-%d/%d%c", &dx_code_1, &dx_code_2, &frame_number, &half_frame_flag) < 3) {
 
         if (sscanf(dx_info, "%d", &dx_extract) < 1){
-            return -1; // invalid format
+            return errtxt(ZINT_ERROR_INVALID_DATA, symbol, 1006, "DX number is incorrect. Expected: 4 digits (DX extract) or 6 digits (DX full).");
+        }
+        if (dx_extract <= 0 || dx_extract > 2047){
+            return errtxtf(ZINT_ERROR_INVALID_DATA, symbol, 1010, "DX extract is %d. Allowed values are between 0 and 2047.", dx_extract);
         }
         printf("DX extract: %04d\n", dx_extract);
         dx_code_1 = dx_extract / 16;
         dx_code_2 = dx_extract % 16;
     }
 
-    if (dx_code_1 <= 0 || dx_code_1 > 127){
-        return -1; // invalid format
-    }
-    if (dx_code_2 < 0 || dx_code_2 > 15){
-        return -1; // invalid format
-    }
+
 
     printf("DX code 1: %d\n", dx_code_1);
     printf("DX code 2: %d\n", dx_code_2);
@@ -181,11 +260,12 @@ int parse_dx_code(const unsigned char *source, char *binary_output, int *output_
 
         // if (*has_frame_info && frame_number >= 0) {
         if (sscanf(frame_info, "%d%c", &frame_number, &half_frame_flag) < 1){
+            return errtxt(ZINT_ERROR_INVALID_DATA, symbol, 1008, "DX part 2 must be between 0 and 15.");
             return -1; // invalid format
         }
         int_to_binary(frame_number, 6, binary_frame_number); // 6 bits for frame_number
         if (frame_number < 0 || frame_number > 63){
-            return -1; // invalid format
+            return errtxt(ZINT_ERROR_INVALID_DATA, symbol, 1009, "Frame number should be between 0 and 63.");
         }
         printf("frame number: %d\n", frame_number);
         printf("binary format: %s\n", binary_frame_number);
@@ -262,12 +342,13 @@ INTERNAL int dxfilmedge(struct zint_symbol *symbol, unsigned char source[], int 
     char char_data[32];
     int data_length;
     bool has_frame_info;
-    if (parse_dx_code(source, char_data, &data_length, &has_frame_info) == 0){
+    int parse_result = parse_dx_code(symbol, source, char_data, &data_length, &has_frame_info);
+    if (parse_result == 0){
         printf("  Input parsed.\n");
     }
     else{
         printf("  Failed to parse.\n\n");
-        return -1;
+        return parse_result;
     }
 
 
